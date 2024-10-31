0a70972051167067e5fdd7e4705ccdde0f1eebea
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2024-10-31 23:25:59 +0200

    add scrollreveal to website

--- theme/static/js/main.js
+++ theme/static/js/main.js
@@ -3,9 +3,9 @@
 const navMenu = document.getElementById('nav-menu')
 const navLink = document.querySelectorAll('.nav-link')
 const hamburger = document.getElementById('hamburger')
+const scrollUpBtn = document.get
 
 hamburger.addEventListener('click', ()=> {
-    console.log('clicked')
     navMenu.classList.toggle('left-0')
     navMenu.classList.toggle('left-[-100%]')
     hamburger.classList.toggle('ri-close-large-fill')
@@ -20,7 +20,7 @@ navLink.forEach( link => {
 })
 
 
-// Swiper
+/* Swiper */
 
 const swiper = new Swiper('.swiper', {
     // Optional parameters
@@ -51,3 +51,94 @@ const swiper = new Swiper('.swiper', {
     }
   
   });
+
+  /*  Scroll Up */
+
+  const scrollUp = () => {
+    const scrollUpBtn = document.getElementById('scroll-up')
+
+    //console.log(this.scrollY)
+
+    if (this.scrollY >= 250) {
+        scrollUpBtn.classList.remove('-bottom-1/2')
+        scrollUpBtn.classList.add('bottom-4')
+    } else {
+        scrollUpBtn.classList.add('-bottom-1/2')
+        scrollUpBtn.classList.remove('bottom-4')
+    }
+
+  }
+
+  window.addEventListener('scroll', scrollUp)
+
+  /* Srroll Header  */
+
+  const scrollHeader = () => {
+    const header = document.getElementById('navbar')
+
+    //console.log(this.scrollY)
+
+    if (this.scrollY >= 50) {
+        header.classList.add('border-b', 'border-yellow-500')
+    } else {
+        header.classList.remove('border-b', 'border-yellow-500')
+    }
+
+  }
+
+  window.addEventListener('scroll', scrollHeader)
+
+  /* Active Link */
+
+  const activeLink = function() {
+    const sections  = document.querySelectorAll('section')
+    //console.log(sections)
+    const navLinks = document.querySelectorAll('.nav-link')
+    //console.log(navLinks) 
+
+    let current = 'home'
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        //console.log(sectionTop)
+      
+        console.log(this.scrollY)
+
+        if (this.scrollY >= sectionTop - 60) {
+            current = section.getAttribute('id')
+            console.log(current)
+        }
+    })
+
+    navLinks.forEach(item => {
+        item.classList.remove("active")
+
+        if (item.href.includes(current)) {
+            item.classList.add('active')
+        }
+    })
+  }
+
+  window.addEventListener('scroll', activeLink)
+
+
+  /* scroll reveal */
+
+  const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 300,
+    reset: true
+  })
+
+  sr.reveal(`.home-data, .about-top, .trending-top, .review-top, .review-swiper, 
+    .footer-icon, .footer-content`)
+  sr.reveal(`.home-image`, {delay: 500, scale: 0.5 })
+  sr.reveal(`.service-card, .popular-card`, { interval:100 })
+
+  sr.reveal(`.about-img_2, .about-content-1`, { origin: 'right' })
+  sr.reveal(`.about-img_1, .about-content-2`, { origin: 'left' })
+
+  sr.reveal('.copyright-text', {delay: 500, origin:'bottom'})
+  sr.reveal('.footer-floral', {delay: 1000, origin:'bottom'})
