9c9890eba26c58291844048e6861fc92c5d5b46f
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-02 04:10:57 +0200

    add categories context to base

--- store/views.py
+++ store/views.py
@@ -17,6 +17,11 @@ class ShopIndexView(TemplateView):
 class HomePageView(TemplateView):
     template_name = "store/home.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["categories"] = Category.objects.all()
+        return context
+
 
 class CategoriesView(ListView):
     template_name = "store/categories.html"
