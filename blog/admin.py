424c878951d0803b7bff10218629fb1f335382c0
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-03 01:59:42 +0200

    add blog app model admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Post, Comment, Author, Topic
 
-# Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+class TopicAdmin(admin.ModelAdmin):
+    pass
+
+# Customizing the Post admin view
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'created_at', 'published']
+    list_filter = ['author', 'created_at', 'published', 'tag']
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    ordering = ['-created_at']
+
+# Customizing the Comments admin view
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['user', 'post', 'created_at', 'published']
+    list_filter = ['post', 'user', 'created_at', 'published']
+    search_fields = ['comment']
+
+
+# Registering models with the admin site
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
