9b1805592f5682f8f6e03a5c0a6b77dd9bd9a9b4
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-03 16:37:40 +0200

    add urlpatterns to blog app for index and category filters

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("blog/", views.BlogHomeView.as_view(), name="blog"), 
-    path("blog/posts/", views.BlogPostsView.as_view(), name="all-posts"),
-    path("blog/posts/<slug:slug>", views.BlogDetailView.as_view(), name="single-post-page"),
-    path("blog/posts/read-later", views.ReadLaterView.as_view(), name="read-later")
+    path('', views.BlogIndexView.as_view(), name='blog'),
+    path('<slug:slug>/', views.BlogTopicsView.as_view(), name='topics'),
 ]
