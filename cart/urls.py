82740b5af3c1ecb621a0833d196af1dc003adcd7
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-03 22:54:30 +0200

    add cart urls and views

---
+++ cart/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path 
+from . import views
+
+urlpatterns = [
+   path('', views.CartSummaryView.as_view(), name='cart-summary'),
+   path('add/', views.CartAddView.as_view(), name='cart-add'),
+   path('delete/', views.CartDeleteView.as_view(), name='cart-delete'),
+   path('update/', views.CartUpdateView.as_view(), name='cart-summary')
+]
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+import json
+from django.shortcuts import redirect, get_object_or_404
+from .cart import Cart
+from store.models import Product
+from django.views import View
+from django.http import JsonResponse
+from django.views.generic import TemplateView, ListView, DetailView
 
 # Create your views here.
+
+class CartSummaryView(TemplateView):
+    template_name = 'cart/cart-index.html'
+    
+class CartAddView(View):
+    def post(self, request, *args, **kwargs):
+        cart = Cart(request)
+
+        try:
+            # Parse the JSON data from the request body
+            data = json.loads(request.body)
+
+            # Access the action, product_id, and product_qty from the parsed data
+            if data.get('action') == 'post':
+                product_id = int(data.get('product_id'))
+                product_qty = int(data.get('product_quantity'))
+                
+                product = get_object_or_404(Product, id=product_id)
+                cart.add(product=product, product_qty=product_qty)
+
+                response = JsonResponse({
+                    'status': 'success',
+                    'product_name': product.title,
+                    'product_qty': product_qty
+                })
+
+                return response
+            else:
+                return JsonResponse({'status': 'error', 'message': 'Invalid action'}, status=400)
+        
+        except (json.JSONDecodeError, TypeError, ValueError) as e:
+            # Handle JSON parsing errors and invalid data types
+            return JsonResponse({'status': 'error', 'message': str(e)}, status=400)
+    
+
+
+class CartDeleteView(TemplateView):
+    template_name = 'cart/cart-index.html'
+
+
+class CartUpdateView(TemplateView):
+    template_name = 'cart/cart-index.html'
