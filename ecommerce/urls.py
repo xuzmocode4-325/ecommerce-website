d9e4af8dbc2705ccf99e5335f2ed3a2c95d75250
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-03 16:38:43 +0200

    update main app url path for blog app

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
-    path('', include('blog.urls')),
+    path('blog/', include('blog.urls')),
     path("__reload__/", include('django_browser_reload.urls')),
 ]
 
