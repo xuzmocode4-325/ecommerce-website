65a08eb3b8c85c3f2d856c045cb2deb9d9649515
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-04 20:31:35 +0200

    update reverse reference to product-info page for more consitencey

--- store/urls.py
+++ store/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
     path('store/', views.StoreIndexView.as_view(), name='store'),
-    path('store/product/<slug:slug>/', views.ProductInfoView.as_view(), name='single-product'),
+    path('store/product/<slug:slug>/', views.ProductInfoView.as_view(), name='product-info'),
     path('store/category/<slug:slug>/', views.CategoryProductsView.as_view(), name='categories'),
 ]
