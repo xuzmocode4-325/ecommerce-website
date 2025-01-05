fa6d9abee46957dd4f05ba1edc787dd0dfbb65e5
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-05 05:29:24 +0200

    add account form data

---
+++ account/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+def register(request):
+    return HttpResponse('Account setup!')
