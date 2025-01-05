3626ad4a720f4b08cc3097841c0f6025b3c22c0d
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-05 03:36:35 +0200

    fix cart update url path

--- cart/urls.py
+++ cart/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
    path('', views.CartSummaryView.as_view(), name='cart-summary'),
    path('add/', views.CartAddView.as_view(), name='cart-add'),
    path('delete/', views.CartDeleteView.as_view(), name='cart-delete'),
-   path('update/', views.CartUpdateView.as_view(), name='cart-summary')
+   path('update/', views.CartUpdateView.as_view(), name='cart-update')
 ]
