3a889a13ad3710e010d2e1ddb578185d1f96aeb5
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-06 03:39:29 +0200

    update url for register view

--- account/urls.py
+++ account/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register', views.register, name='register')
+    path('register', views.UserRegisterView.as_view(), name='register')
 ]
