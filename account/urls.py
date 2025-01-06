0e07411f0a97fe7f622085e278b484a17c24c0a2
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-06 09:09:17 +0200

    update account urls

--- account/urls.py
+++ account/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('register', views.UserRegisterView.as_view(), name='register'),
-    path('email-verification', views.EmailVerificationView.as_view(), name='register'),
+    path('email-verification/<str:uidb64>/<str:token>', views.EmailVerificationView.as_view(), name='register'),
     path('email-verification-sent', views.EmailVerificationSentView.as_view(), name='register'),
     path('email-verification-fail', views.EmailVerificationFailView.as_view(), name='register'),
     path('email-verified', views.EmailVerifiedView.as_view(), name='register'),
