03aac98ae5febc2ce2574aff3eb860a003e28e26
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-07 00:17:50 +0200

    update and fix path names in account url patterns

--- account/urls.py
+++ account/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('register', views.UserRegisterView.as_view(), name='register'),
-    path('email-verification/<str:uidb64>/<str:token>', views.EmailVerificationView.as_view(), name='register'),
-    path('email-verification-sent', views.EmailVerificationSentView.as_view(), name='register'),
-    path('email-verification-fail', views.EmailVerificationFailView.as_view(), name='register'),
-    path('email-verified', views.EmailVerifiedView.as_view(), name='register'),
+    path('email-verification/<str:uidb64>/<str:token>', views.EmailVerificationView.as_view(), name='email-verification'),
+    path('email-verification-sent', views.EmailVerificationSentView.as_view(), name='email-verification-sent'),
+    path('email-verification-fail', views.EmailVerificationFailView.as_view(), name='email-verification-fail'),
+    path('email-verified', views.EmailVerifiedView.as_view(), name='email-verified'),
 ]
