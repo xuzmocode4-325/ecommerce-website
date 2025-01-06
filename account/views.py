8e7351418245c9884065906ef19b5dfe465f0473
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-06 07:23:36 +0200

    add user registration view

--- account/views.py
+++ account/views.py
@@ -1,8 +1,27 @@
+
+from django.urls import reverse_lazy
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
+from . forms import CreateUserForm
+import logging
+
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 
-class UserRegisterView(TemplateView):
+class UserRegisterView(FormView):
     template_name = 'account/registration/register.html'
+    success_url = reverse_lazy('store')
+    form_class = CreateUserForm
+
+    def form_valid(self, form):
+        try:
+            form.save()
+            logger.info('Form saved successfully.')
+
+            return super().form_valid(form)
+
+        except Exception as e:
+            logger.exception(f'An error occurred in form_valid: {e}')
+            return render(self.request, 'index/error_500.html', status=500)
