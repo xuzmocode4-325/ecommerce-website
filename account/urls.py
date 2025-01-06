0040df8d10c5781fe4d0c8a28ff561a1a185a8fe
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-06 07:47:17 +0200

    add email verifyer urls

--- account/urls.py
+++ account/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register', views.UserRegisterView.as_view(), name='register')
+    path('register', views.UserRegisterView.as_view(), name='register'),
+    path('email-verification', views.EmailVerificationView.as_view(), name='register'),
+    path('email-verification-sent', views.EmailVerificationSentView.as_view(), name='register'),
+    path('email-verification-fail', views.EmailVerificationFailView.as_view(), name='register'),
+    path('email-verified', views.EmailVerifiedView.as_view(), name='register'),
 ]
