c81f9eaf5feafe899079709f9f4c1159c30143ab
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-08 07:46:08 +0200

    add login url

--- account/urls.py
+++ account/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('email-verification-sent', views.EmailVerificationSentView.as_view(), name='email-verification-sent'),
     path('email-verification-fail', views.EmailVerificationFailView.as_view(), name='email-verification-fail'),
     path('email-verified', views.EmailVerifiedView.as_view(), name='email-verified'),
+    path('login', views.UserLoginView.as_view(), name='login')
 ]
