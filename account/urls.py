839ffd6260e5c61bd9afc4db1b6efabe6a71a963
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-08 09:22:55 +0200

    add logout path to account url patterns

--- account/urls.py
+++ account/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('email-verification-sent', views.EmailVerificationSentView.as_view(), name='email-verification-sent'),
     path('email-verification-fail', views.EmailVerificationFailView.as_view(), name='email-verification-fail'),
     path('email-verified', views.EmailVerifiedView.as_view(), name='email-verified'),
-    path('login', views.UserLoginView.as_view(), name='login'),
+    path('login', views.LoginView.as_view(), name='login'),
+    path('logout', views.LogoutView.as_view(), name='logout'),
     path('dashboard', views.DashboardView.as_view(), name='dashboard')
 ]
