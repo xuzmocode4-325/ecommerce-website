c1521bc9434791631daae770a446627d31da86cc
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-08 08:06:41 +0200

    add login and dashboard urls to account app

--- account/urls.py
+++ account/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('email-verification-sent', views.EmailVerificationSentView.as_view(), name='email-verification-sent'),
     path('email-verification-fail', views.EmailVerificationFailView.as_view(), name='email-verification-fail'),
     path('email-verified', views.EmailVerifiedView.as_view(), name='email-verified'),
-    path('login', views.UserLoginView.as_view(), name='login')
+    path('login', views.UserLoginView.as_view(), name='login'),
+    path('dashboard', views.DashboardView.as_view(), name='dashboard')
 ]
