ca14396fe70007ed282680b09cbc55ea87f00c4f
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-09 08:47:44 +0200

    add email validation

--- account/forms.py
+++ account/forms.py
@@ -6,16 +6,6 @@ from django import forms
 # Registration Form
 class CreateUserForm(UserCreationForm):
      
-    email = forms.EmailField(
-        label='Email Address',
-        help_text='Required. No spam.',
-        widget=forms.EmailInput(
-            attrs={
-                'class': 'form-input',
-            }
-        ),
-        required=True,
-    )
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
@@ -36,6 +26,17 @@ class CreateUserForm(UserCreationForm):
                 }
             ),
         }
+
+    email = forms.EmailField(
+        label='Email Address',
+        help_text='Required. No spam.',
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'form-input',
+            }
+        ),
+        required=True,
+    )
 
     def __init__(self, *args, **kwargs):
         super(CreateUserForm, self).__init__(*args, **kwargs)
@@ -62,9 +63,34 @@ class LoginForm(AuthenticationForm):
 
 
 class UpdateUserForm(forms.ModelForm):
-    password = None
-
     class Meta: 
         model = User
         fields = ['username', 'email']
         exclude = ['password1', 'password2']
+        widgets = {
+            'username': forms.TextInput(
+                attrs={
+                    'class': 'form-input',
+                }
+            ),
+        }
+
+    password = None
+
+    email = forms.EmailField(
+        label='Email Address',
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'form-input',
+            }
+        ),
+        required=True,
+    )
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+
+        if User.objects.filter(email=email).exclude(pk=self.instance.pk).exists():
+            raise forms.ValidationError('An account with this email already exists.')
+        
+        return email
