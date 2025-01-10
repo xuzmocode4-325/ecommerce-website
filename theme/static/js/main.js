0dbc5c540a39dfcb5e9d1ce0f5aa23e38d78ddc2
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-10 11:08:09 +0200

    update styles and js

--- theme/static/js/main.js
+++ theme/static/js/main.js
@@ -89,17 +89,18 @@ breakpoints: {
 
 /*  Scroll Up */
 const scrollUp = () => {
-const scrollUpBtn = document.getElementById('scroll-up')
-
-if (this.scrollY >= 250) {
-    scrollUpBtn.classList.remove('-bottom-1/2')
-    scrollUpBtn.classList.add('bottom-4')
-
-} else {
-    scrollUpBtn.classList.add('-bottom-1/2')
-    scrollUpBtn.classList.remove('bottom-4')
-}
-
+    const scrollUpBtn = document.getElementById('scroll-up')
+    if (scrollUpBtn) {
+        if (this.scrollY >= 250) {
+            scrollUpBtn.classList.remove('-bottom-1/2')
+            scrollUpBtn.classList.add('bottom-4')
+    
+        } else {
+            scrollUpBtn.classList.add('-bottom-1/2')
+            scrollUpBtn.classList.remove('bottom-4')
+        }
+    
+    }
 }
 
 window.addEventListener('scroll', scrollUp)
