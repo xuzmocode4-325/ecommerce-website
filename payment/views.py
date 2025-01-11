5314bdee2f0e779fa729d6538025703681451c57
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-11 14:18:49 +0200

    add payment view file

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+class PaymentSuccessView(TemplateView):
+    template_name = 'payment/payment-success.html'
+
+
+class PaymentFailView(TemplateView):
+    template_name = 'payment/payment-fail.html'
