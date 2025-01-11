f7b82cbdde5049f3912831ee11902ec73efd6ca2
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-11 14:18:09 +0200

    add shipping addresses model

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class ShippingAddress(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=255)
+    address1 = models.CharField(max_length=300)
+    address2 = models.CharField(max_length=300)
+    city = models.CharField(max_length=255)
+    #optional
+    state = models.CharField(max_length=255, null=True, blank=True)
+    zipcode = models.CharField(max_length=255, null=True, blank=True)
+    #fk 
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Shipping Address'
+
+    def __str__(self):
+        return 'Shipping Address - ' + str(self.id)
