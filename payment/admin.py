8d704c87566c774aa2a7fc6d5543dec339662316
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-11 14:23:51 +0200

    add register shipping model to payment app admin

--- payment/admin.py
+++ payment/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ShippingAddress
 
 # Register your models here.
+
+class ShippingAddressAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(ShippingAddress, ShippingAddressAdmin)
