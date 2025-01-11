5de633e1eafa41081ba6abba7bf475af14cf9a4e
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-11 13:51:21 +0200

    add acount app to site settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -77,7 +77,8 @@ INSTALLED_APPS = [
     'cart', # django app
     'theme', # django app
     'store', # django app
-    'account', #django app
+    'account', # django app
+    'payment', # django app
     'tailwind', # django package
     'mathfilters', # django package
     'django_browser_reload',
