6a78b4168c4642464482642f030125411237a82b
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-12 13:55:01 +0200

    create shipping data form

--- payment/forms.py
+++ payment/forms.py
@@ -2,8 +2,47 @@ from django import forms
 from . models import ShippingAddress
 
 class ShippingForm(forms.ModelForm):
+       
     class Meta:
         model = ShippingAddress
         fields = ['name', 'surname', 'email', 'address1', 'address2', 'city', 'state', 'zipcode']
-        exclude = ['user']
+        exclude = ['user', 'email']
+        widgets = {
+            'name': forms.TextInput(
+                attrs={
+                    'class': 'form-input',
+                }
+            ),
+            'surname': forms.TextInput(
+                attrs={
+                    'class': 'form-input',
+                }
+            ),
+            'address1': forms.TextInput(
+                attrs={
+                    'class': 'form-input',
+                }
+            ),
+            'address2': forms.TextInput(
+                attrs={
+                    'class': 'form-input',
+                }
+            ),
+            'city': forms.TextInput(
+                attrs={
+                    'class': 'form-input',
+                }
+            ),
+            'state': forms.TextInput(
+                attrs={
+                    'class': 'form-input',
+                }
+            ),
+            'zipcode': forms.TextInput(
+                attrs={
+                    'class': 'form-input',
+                }
+            ),
+        }
+
 
