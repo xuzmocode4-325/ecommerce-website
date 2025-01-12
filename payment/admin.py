c147090d93a4c49090a4423d2e97f5af21987645
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-12 13:54:47 +0200

    prepopulate shipping data with user email

--- payment/admin.py
+++ payment/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 from .models import ShippingAddress
 
 # Register your models here.
 
 class ShippingAddressAdmin(admin.ModelAdmin):
-    pass
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        # Prepopulate the email field with the current user's email
+        if request.user.is_authenticated:
+            form.base_fields['email'].initial = request.user.email
+        return form
 
 admin.site.register(ShippingAddress, ShippingAddressAdmin)
