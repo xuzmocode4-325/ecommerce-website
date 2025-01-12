e067febd12c05d84c7e0699bc37b92656efb9316
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-12 17:28:09 +0200

    add checkout view

--- payment/views.py
+++ payment/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from . models import ShippingAddress
 
 # Create your views here.
 
@@ -12,3 +13,15 @@ class PaymentFailView(TemplateView):
 
 class CheckoutView(TemplateView):
     template_name = 'payment/checkout.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            try:
+                context['shipping'] = ShippingAddress.objects.get(
+                    user=self.request.user.id
+                )
+                return context
+            except:
+                return context
+        return context
