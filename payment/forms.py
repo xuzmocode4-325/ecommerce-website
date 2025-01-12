6e3526149d31f0b137df7ddea5a234f2ddb72a5b
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-12 18:02:32 +0200

    update form placeholders and labels for shipping form

--- payment/forms.py
+++ payment/forms.py
@@ -2,47 +2,23 @@ from django import forms
 from . models import ShippingAddress
 
 class ShippingForm(forms.ModelForm):
-       
     class Meta:
         model = ShippingAddress
         fields = ['name', 'surname', 'email', 'address1', 'address2', 'city', 'state', 'zipcode']
         exclude = ['user', 'email']
         widgets = {
-            'name': forms.TextInput(
-                attrs={
-                    'class': 'form-input',
-                }
-            ),
-            'surname': forms.TextInput(
-                attrs={
-                    'class': 'form-input',
-                }
-            ),
-            'address1': forms.TextInput(
-                attrs={
-                    'class': 'form-input',
-                }
-            ),
-            'address2': forms.TextInput(
-                attrs={
-                    'class': 'form-input',
-                }
-            ),
-            'city': forms.TextInput(
-                attrs={
-                    'class': 'form-input',
-                }
-            ),
-            'state': forms.TextInput(
-                attrs={
-                    'class': 'form-input',
-                }
-            ),
-            'zipcode': forms.TextInput(
-                attrs={
-                    'class': 'form-input',
-                }
-            ),
+            'name': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Bonnie'}),
+            'surname': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Green'}),
+            'address1': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Your Street'}),
+            'address2': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Your Suburb'}),
+            'city': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Your City'}),
+            'state': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Your State / Province'}),
+            'zipcode': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Your Zip / Area Code'}),
+        }
+        labels = {
+            'address1': 'Street Address',
+            'address2': 'Suburb',
+            'state': 'State / Province'
         }
 
 
