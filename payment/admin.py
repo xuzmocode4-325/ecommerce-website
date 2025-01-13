5d242f92b807f0dfefb73b1eb9dbbdbdf0c907d6
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-13 06:19:42 +0200

    add admin for order and order item model

--- payment/admin.py
+++ payment/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
-from .models import ShippingAddress
+from .models import ShippingAddress, Order, OrderItem
 
 # Register your models here.
 
@@ -11,5 +11,14 @@ class ShippingAddressAdmin(admin.ModelAdmin):
         if request.user.is_authenticated:
             form.base_fields['email'].initial = request.user.email
         return form
+    
+class OrderAdmin(admin.ModelAdmin):
+    pass
+
+class OrderItemAdmin(admin.ModelAdmin):
+    pass
+
 
 admin.site.register(ShippingAddress, ShippingAddressAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
