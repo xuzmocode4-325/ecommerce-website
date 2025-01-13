8adda03eeb19f6511de93bfb24c79c469e3b2783
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-13 04:27:34 +0200

    add order and order item models to payment app

--- payment/models.py
+++ payment/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from store.models import Product
+from django_countries.fields import CountryField
 
 # Create your models here.
 
@@ -14,6 +16,7 @@ class ShippingAddress(models.Model):
     #optional
     state = models.CharField(max_length=255, null=True, blank=True)
     zipcode = models.CharField(max_length=255, null=True, blank=True)
+    country = CountryField(null=True, blank=True, blank_label="(Select Country)")
     #fk 
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
@@ -22,3 +25,26 @@ class ShippingAddress(models.Model):
 
     def __str__(self):
         return 'Shipping Address - ' + str(self.id)
+    
+class Order(models.Model): 
+    full_name = models.CharField(max_length=300)
+    email = models.EmailField(max_length=255)
+    shipping_address = models.TextField(max_length=10000)
+    amount_paid = models.DecimalField(max_digits=8, decimal_places=2)
+    date_ordered = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return 'Order - #' + str(self.id)
+    
+
+class OrderItem(models.Model): 
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    quantity = models.PositiveBigIntegerField(default=1)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    
+    def __str__(self):
+        return 'Order Item - #' + str(self.id)
+
