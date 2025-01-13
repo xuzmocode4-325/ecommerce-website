15396d55183ebf1502fd3b752c123e152b4f96c2
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-13 06:20:28 +0200

    add urls for order completion

--- payment/urls.py
+++ payment/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('payment-success', views.PaymentSuccessView.as_view(), name='payment-success'),
     path('payment-failed', views.PaymentFailView.as_view(), name='payment-failed'),
-    path('checkout', views.CheckoutView.as_view(), name='checkout')
+    path('checkout', views.CheckoutView.as_view(), name='checkout'),
+    path('complete-order', views.CompleteOrderView.as_view(), name='complete-order')
 ]
