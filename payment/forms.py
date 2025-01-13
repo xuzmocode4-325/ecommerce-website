96042758452bbe1105ddfbff6294e7b44576097c
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-13 04:26:18 +0200

    add country field to shipping address model form

--- payment/forms.py
+++ payment/forms.py
@@ -4,7 +4,7 @@ from . models import ShippingAddress
 class ShippingForm(forms.ModelForm):
     class Meta:
         model = ShippingAddress
-        fields = ['name', 'surname', 'email', 'address1', 'address2', 'city', 'state', 'zipcode']
+        fields = ['name', 'surname', 'email', 'address1', 'address2', 'city', 'state', 'country','zipcode']
         exclude = ['user', 'email']
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Bonnie'}),
