91f019afb94a4ce6535e6fa94de96a58a7acd1a6
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-15 06:21:41 +0200

    add payment handler script

--- payment/static/payment/js/handle-payment.js
+++ payment/static/payment/js/handle-payment.js
@@ -29,69 +29,7 @@ const csrfToken = scriptTag.dataset.csrfToken;
 const completeOrder = async (event) => {
     event.preventDefault();
 
-    const firstname = document.getElementById('firstname').value;
-    const surname = document.getElementById('surname').value;
-    const email = document.getElementById('email').value;
-    const address1 = document.getElementById('address1').value;
-    const address2 = document.getElementById('address2').value;
-    const city = document.getElementById('city').value;
-    const state = document.getElementById('state').value;
-    const country = document.getElementById('country').value; 
-    const zipcode = document.getElementById('zipcode').value; 
-
-    console.log('Firstname:', firstname);
-    console.log('Surname:', surname);
-    console.log('Email:', email);
-    console.log('Address1:', address1);
-    console.log('Address2:', address2);
-    console.log('City:', city);
-    console.log('State:', state);
-    console.log('Country:', country);
-    console.log('Zipcode:', zipcode);
-
-    try {
-        /**
-         * Sends a POST request to add the product to the cart.
-         * @type {Response}
-         */
-        const response = await fetch(cartSubmitUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-CSRFToken': csrfToken
-            },
-            body: JSON.stringify({
-                fn: firstname, 
-                sn: surname, 
-                em: email, 
-                ad1: address1, 
-                ad2: address2, 
-                ct: city, 
-                st: state, 
-                cntry: country, 
-                zip: zipcode
-            })
-        });
-
-        if (!response.ok) {
-            console.log(response)
-            window.location.replace('payment-failed')
-            throw new Error('Network response was not ok');
-            
-        } else {
-            /**
-             * Parses the JSON response from the server.
-             * @type {Object}
-             */
-            const json = await response.json();
-            //console.log(json); 
-    
-            window.location.replace('payment-success')
-        }
-    } catch (error) {
-        //window.location.replace("{% url 'payment-failed' %}")
-        console.error('There was a problem with the fetch operation:', error);
-    }
+   
 };
 
 // Adds an event listener to the add button to trigger the addToCart function on click.
