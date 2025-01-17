8483972731356560ecc880dd507c29f5b2caa03d
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-17 05:46:03 +0200

    add coupon model

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Coupon(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    discount = models.DecimalField(max_digits=5, decimal_places=2)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.discount * 100}%"
+    
